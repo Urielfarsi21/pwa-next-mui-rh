f6e9725d9217b3664ee4b9d6111f78e869d66363
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-08-10 02:57:41 -0300

    login implement

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,7 @@ import { Box } from "@mui/material";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <Box display={"flex"} flexDirection="column" minHeight={"100vh"}>
+    <Box minHeight={"100vh"}>
       <Header />
       <Main>{children}</Main>
       <Footer />
--- src/layout/components/Main.tsx
+++ src/layout/components/Main.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React, { ReactNode } from "react";
 
 const Main = ({ children }: { children: ReactNode }) => {
-  return <Box flexGrow={1}>{children}</Box>;
+  return <Box minHeight={"92vh"}>{children}</Box>;
 };
 
 export default Main;
