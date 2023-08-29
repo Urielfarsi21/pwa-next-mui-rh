9ca14cd423163f38c7fae77bd607c9a55dba6204
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-08-29 04:23:03 -0300

    implement useQuery

--- pages/planes.tsx
+++ pages/planes.tsx
@@ -1,36 +1,19 @@
 import Plans from "@/src/planes/Plans";
-import { Plan } from "@/src/planes/types/Plan";
+import { usePlanes } from "@/src/planes/hooks/usePlanes";
+import { LinearProgress } from "@mui/material";
 
 // api plans
 // https://mocki.io/v1/819ad59a-d91e-45dd-9820-30f2939b3e34
-export const plans: Plan[] = [
-  {
-    title: "Website",
-    description: "Crea un catálogo profesional y atractivo que sorprenda.",
-    price: 2990,
-    hrefViewMore: "#",
-    hrefSelectPlan: "/comprar/0",
-  },
-  {
-    title: "Business",
-    description:
-      "Comienza a vender al instante con los elementos esenciales del eCommerce.",
-    price: 5990,
-    hrefViewMore: "#",
-    hrefSelectPlan: "/comprar/1",
-  },
-  {
-    title: "Advanced Store",
-    description:
-      "Haz crecer tu negocio con potentes herramientas de marketing.",
-    price: 8990,
-    hrefViewMore: "#",
-    hrefSelectPlan: "/comprar/2",
-  },
-];
 
 const planes = () => {
-  return <Plans plans={plans} />;
+  const planes = usePlanes();
+  return (
+    <>
+      {planes.isLoading && <LinearProgress />}
+      {planes.isError && <div>error...</div>}
+      {planes.isSuccess && <Plans plans={planes.data} />}
+    </>
+  );
 };
 
 export default planes;
