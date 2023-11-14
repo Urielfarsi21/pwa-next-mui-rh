d114bcc77744be91979464c8d98e6012a1eb7115
Uriel <135763366+Urielfarsi21@users.noreply.github.com>
2023-11-14 22:47:56 +0000

    Update 4 files

--- pages/Rooms/[id].tsx
+++ pages/Rooms/[id].tsx
@@ -12,52 +12,77 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 export default function MediaCard() {
   return (
     <>
-      <Button size="small">Stock</Button>
-      <Button size="small">Sin Stock</Button>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-          title="Cocina"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Cocina
-          </Typography>
-          <Typography variant="body2" color="text.secondary"></Typography>
-        </CardContent>
-        <CardActions></CardActions>
-      </Card>
-      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <nav aria-label="main mailbox folders">
+
+      <Box
+        sx={{
+          backgroundColor: "#414141",
+          color: "white",
+        }}
+      >
+        <Stack justifyContent={"center"} direction="row" marginTop={5}>
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Sin Stock</Button>
+            <Button>Con Stock</Button>
+          </ButtonGroup>
+        </Stack>
+      </Box>
+      <Stack spacing={2} alignItems={"center"}>
+        <Card sx={{}}>
+          <CardMedia
+            sx={{ height: 140, width: 290 }}
+            image="https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+          />
           <List>
             <ListItem disablePadding>
               <ListItemButton>
                 <FavoriteBorderIcon>
                   <InboxIcon />
                 </FavoriteBorderIcon>
-                <ListItemText primary="Tomates" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <FavoriteBorderIcon>
-                  <DraftsIcon />
-                </FavoriteBorderIcon>
-                <ListItemText primary="Papas" />
+                <ListItemText primary="Cocina" />
               </ListItemButton>
             </ListItem>
           </List>
-        </nav>
-        <Divider />
-      </Box>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div"></Typography>
+            <Typography variant="body2" color="text.secondary"></Typography>
+          </CardContent>
+          <CardActions></CardActions>
+        </Card>
+        <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <FavoriteBorderIcon>
+                    <InboxIcon />
+                  </FavoriteBorderIcon>
+                  <ListItemText primary="Tomates" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <FavoriteBorderIcon>
+                    <DraftsIcon />
+                  </FavoriteBorderIcon>
+                  <ListItemText primary="Papas" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
+      </Stack>
     </>
   );
 }
--- pages/productopag/[id].tsx
+++ pages/productopag/[id].tsx
@@ -1,5 +1,9 @@
 import ListSubheader from "@mui/material/ListSubheader";
 import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -21,6 +25,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 export default function DenseAppBar() {
   const [open, setOpen] = React.useState(true);
@@ -30,71 +35,80 @@ export default function DenseAppBar() {
   };
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" component="div">
-              Producto
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://www.carniceriatomasello.com.ar//website/cms-images/10/resize"
-            alt="Milanesa"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Milanesa
-            </Typography>
-            <Typography variant="body2" color="text.secondary"></Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <List
-        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            Nested List Items
-          </ListSubheader>
-        }
-      >
-        <ListItemButton>
-          <ListItemIcon></ListItemIcon>
-          <ListItemText primary="Fecha de vencimimento 25/8/2000" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon></ListItemIcon>
-          <ListItemText primary="Fecha de stock 25/8/1999" />
-        </ListItemButton>
-        <ListItemButton onClick={handleClick}>
-          <ListItemIcon></ListItemIcon>
-          <ListItemText primary="Otras Ubicaciones" />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon></ListItemIcon>
-              <ListItemText primary="Reponer Producto" />
-            </ListItemButton>
-          </List>
-        </Collapse>
-      </List>
+      <Stack spacing={2} alignItems={"center"}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" component="div">
+                Estado del producto
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://www.carniceriatomasello.com.ar//website/cms-images/10/resize"
+              alt="Milanesa"
+            />
+            <CardContent>
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <FavoriteBorderIcon>
+                      <InboxIcon />
+                    </FavoriteBorderIcon>
+                    <ListItemText primary="Milanesa" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+              <Typography variant="body2" color="text.secondary"></Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Nested List Items
+            </ListSubheader>
+          }
+        >
+          <ListItemButton>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary="Fecha de vencimimento 25/8/2000" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary="Fecha de stock 25/8/1999" />
+          </ListItemButton>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary="Otras Ubicaciones" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon></ListItemIcon>
+                <ListItemText primary="Reponer Producto" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
+      </Stack>
     </>
   );
 }
